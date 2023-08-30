982bb3caf96eb9146b09f2a3db7e39cc20efe21d
dragonnomada <dragonnomada123@gmail.com>
2023-08-29 20:20:26 -0600

    Actualización de la tarea en asyncio

--- d105/c15_asyncio_example_generate_data.py
+++ d105/c15_asyncio_example_generate_data.py
@@ -7,8 +7,11 @@ def task_generate_data(n):
     return time() - start
 
 def task_generate_first_2k_sync():
+    times = []
     for i in range(1, 2_000):
-        task_generate_data(i)
+        time = task_generate_data(i)
+        times.append(time)
+    return times
 
 def task_generate_first_2k_async():
     from concurrent.futures import ThreadPoolExecutor
@@ -26,30 +29,33 @@ def task_generate_first_2k_async():
 
     return [future.result() for future in futures]
 
-def task_generate_first_2k_asyncio():
+async def task_generate_first_2k_asyncio():
     import asyncio
 
     from concurrent.futures import ThreadPoolExecutor
 
-    executor1 = ThreadPoolExecutor(max_workers=4)
+    executor1 = ThreadPoolExecutor(max_workers=2)
 
     loop1 = asyncio.get_event_loop()
     
-    times = []
+    futures = []
 
     for i in range(1, 2_000):
-        time = loop1.run_in_executor(executor1, task_generate_data, i)
-        times.append(time)
+        future = loop1.run_in_executor(executor1, task_generate_data, i)
+        futures.append(future)
 
-    # from concurrent.futures import wait
+    times = []
+    for future in futures:
+        await asyncio.ensure_future(future, loop=loop1)
+        times.append(future.result())
 
-    # wait(futures)
-
-    # return [future.result() for future in futures]
     return times
 
+# times = task_generate_first_2k_sync()
 # times = task_generate_first_2k_async()
-times = task_generate_first_2k_async()
+import asyncio
+loop2 = asyncio.get_event_loop()
+times = loop2.run_until_complete(task_generate_first_2k_asyncio())
 
 print(sum(times) / len(times))
 
